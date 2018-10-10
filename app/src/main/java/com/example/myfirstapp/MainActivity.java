a1791774ab4b9eb2ed6a2b6ef6a80dd7883ef928
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-10-10 00:49:31 -0700

    final commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TOTAL_COUNT = "total_count";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,44 @@ public class MainActivity extends AppCompatActivity {
      */
     public void toastMe(View view)
     {
+        TextView showCountTextView = (TextView)findViewById(R.id.textView);
+
+        showCountTextView.setText("0");
+
         // Toast myToast = Toast.makeText(this, message, duration);
-        Toast myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT);
+        Toast myToast = Toast.makeText(this, "Reset count to 0", Toast.LENGTH_SHORT);
         myToast.show();
+    }
+
+    public void countMe(View view)
+    {
+        // get the text view
+        TextView showCountTextView = (TextView)findViewById(R.id.textView);
+
+        // get the value of the text view, convert to int, increment it
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // display in text view
+        showCountTextView.setText(count.toString());
+    }
+
+
+    public void randomMe(View view)
+    {
+        TextView showCountTextView = (TextView)findViewById(R.id.textView);
+
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+
+        // create an intent to start second activity
+        Intent randomIntent = new Intent(this, SecondActivity.class);
+
+        randomIntent.putExtra(TOTAL_COUNT, count);
+
+        // start new activity
+        startActivity(randomIntent);
+
     }
 }
